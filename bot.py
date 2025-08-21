044a874166c4a4df466cbd5a5872560af3bde7ce
3ejjad <amosajjad92@gmail.com>
2025-08-21 19:45:29 +0330

    Create bot.py
    telegram-bot/
    │
    ├─ bot.py            # فایل اصلی ربات
    ├─ config.py         # تنظیمات و توکن
    ├─ handlers/         # پوشه هندلرها
    │   ├─ start.py      # دستور /start
    │   ├─ help.py       # دستور /help
    │   ├─ echo.py       # پاسخ ساده یا سایر قابلیت‌ها
    └─ requirements.txt  # کتابخانه‌ها

---
+++ bot.py
@@ -0,0 +1,9 @@
+telegram-bot/
+│
+├─ bot.py            # فایل اصلی ربات
+├─ config.py         # تنظیمات و توکن
+├─ handlers/         # پوشه هندلرها
+│   ├─ start.py      # دستور /start
+│   ├─ help.py       # دستور /help
+│   ├─ echo.py       # پاسخ ساده یا سایر قابلیت‌ها
+└─ requirements.txt  # کتابخانه‌ها
